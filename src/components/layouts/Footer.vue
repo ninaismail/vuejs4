a6f64e269b1c0860b8d82f444959d88a5c46d91a
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-09-11 13:52:40 +0300

    another edit

--- src/components/layouts/Footer.vue
+++ src/components/layouts/Footer.vue
@@ -17,20 +17,20 @@ const footerlinks = [
       <div class="relative flex flex-col justify-center w-full bg-center bg-cover bg-[#646565] md:w-1/2">
           <div class="flex justify-center gap-3 p-10">
             <div class="space-y-10">
-              <div class="space-y-3">
+              <div class="space-y-6">
                 <h1 class="text-offwhite 2xl:text-xl lg:text-lg md:text-md font-[700]">Office Location</h1>
                 <p class="cursor-pointer font-[500] 2xl:text-xl lg:text-lg md:text-md text-offwhite lg:w-2/3">
                   UAE, Abu Dhabi, Addax tower, Al reem island, office 3003
               </p>                
               </div>
-              <div class="space-y-3">
+              <div class="space-y-6">
                 <h1 class="text-offwhite 2xl:text-xl lg:text-lg md:text-md font-[700]">Email</h1>
                 <a prefetch="false" aria-label="email us" href="mailto:info@ibiholding.com"   
                 class="cursor-pointer font-[500] 2xl:text-xl lg:text-lg md:text-md text-offwhite hover:brightness-125">
                   info@ibiholding.com
                 </a>                
               </div>
-              <div class="space-y-3">
+              <div class="space-y-6">
                 <h1 class="text-offwhite 2xl:text-xl lg:text-lg md:text-md font-[700]">Phone</h1>
                 <a prefetch="false" aria-label="email us" href="tel:"   
                 class="cursor-pointer font-[500] 2xl:text-xl lg:text-lg md:text-md text-offwhite hover:brightness-125">
@@ -38,7 +38,7 @@ const footerlinks = [
               </a>                
               </div>
             </div> 
-            <div class="space-y-3">
+            <div class="space-y-6">
               <h1 class="text-offwhite 2xl:text-xl lg:text-lg md:text-md font-[700]">Menu</h1>
               <RouterLink v-for="(item, key) in footerlinks" :key="key" :id="item.name" :aria-label="'go to' + item.name" :to="item.to"   
               class="block cursor-pointer font-[500] 2xl:text-xl lg:text-lg md:text-md text-offwhite hover:brightness-125">
