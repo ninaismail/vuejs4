422c10a4d682a58f80b73032594fb4b80231c4dc
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-09-11 14:31:19 +0300

    ...

--- src/components/Commitment.vue
+++ src/components/Commitment.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="flex flex-col justify-around w-11/12 gap-10 py-40 mx-auto 2xl:8/12 sm:w-10/12">
         <div class="flex flex-col gap-3 mx-auto">
-        <h1 class="text-offwhite font-[600] 2xl:text-8xl lg:text-7xl md:text-6xl sm:text-5xl text-4xl tracking-wide">Our Commitment to a Sustainable Future</h1>
+        <h1 class="text-offwhite font-[600] 2xl:text-8xl lg:text-7xl md:text-6xl sm:text-5xl text-4xl tracking-wide">Our Commitment to a <br class="hidden lg:block"/> Sustainable Future</h1>
         <p class="text-offwhite font-[500] 2xl:text-2xl lg:text-xl md:text-lg sm:text-md text-justify">Our responsibility goes beyond financial returns. We are dedicated to creating a future where innovation and sustainability are inseparably connected Our approach is rooted in the belief that true progress is only achieved when it benefits people, the planet, and future generations.</p>
        </div>
        <div class="flex flex-col gap-6 mx-auto">
