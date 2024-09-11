6c3411073a85d2bc6ee9441efee0327f0ee49aae
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-09-11 11:38:16 +0300

    more responsive fixes

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,6 +1,6 @@
 <template>
-    <section class="pt-60 bg-cener bg-cover bg-primary/10 h-[85vh] border-white border-b-[2px] rounded-b-[160px]">
-       <div class="flex flex-col justify-around w-11/12 gap-3 mx-auto 2xl:8/12 sm:w-10/12">
+    <section class="pt-60 bg-cener bg-cover bg-primary/10 min-h-[85vh] border-white border-b-[2px] rounded-b-[160px]">
+       <div class="flex flex-col justify-around w-11/12 gap-3 pb-20 mx-auto 2xl:8/12 sm:w-10/12">
         <h1 class="text-white font-[900] 2xl:text-9xl lg:text-8xl md:text-7xl sm:text-6xl text-5xl appurcase">IBI Holding</h1>
         <hr class="h-32 w-[2px] ms-2 bg-primary border-0"/>
         <h2 class="text-white font-[600] 2xl:text-8xl lg:text-7xl md:text-6xl sm:text-5xl text-4xl tracking-wide">Creating Impact By Innovation</h2>
@@ -9,7 +9,6 @@
         frontiers, we reimagine what technology can achieve, leading the way for transformative solutions and
         shared stakeholders’ value</p>
        </div>
-
     </section>
 </template>
 
