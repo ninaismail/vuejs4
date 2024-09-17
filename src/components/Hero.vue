15f9c63961854044029236ccbafbb6c0f768ac2f
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-09-17 15:47:26 +0300

    ...

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -5,7 +5,7 @@
         <h1 class="text-offwhite font-[900] 2xl:text-8xl lg:text-7xl md:text-6xl sm:text-5xl text-4xl uppercase">IBI Holding</h1>
         <hr class="sm:h-30 h-12 w-[2px] ms-2 bg-primary border-0"/>
         <h2 class="text-offwhite font-[600] 2xl:text-[68px] lg:text-7xl md:text-6xl sm:text-5xl text-4xl tracking-wide">Investing in Tomorrow’s Breakthroughs</h2>
-        <p class="text-offwhite font-[500] 2xl:text-[22px] lg:text-xl md:text-lg sm:text-md lg:w-9/12">We drive the next wave of progress by investing in technologies that change the world. Our investments focus on identifying and nurturing the technologies that will define the future, empowering innovators who dare to challenge the status quo.</p>
+        <p class="text-offwhite font-[500] 2xl:text-[22px] lg:text-xl md:text-lg sm:text-md lg:w-8/12">We drive the next wave of progress by investing in technologies that change the world. Our investments focus on identifying and nurturing the technologies that will define the future, empowering innovators who dare to challenge the status quo.</p>
       </div>
     </div>
   </section>   
