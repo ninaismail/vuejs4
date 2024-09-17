5d4a0bd0e2804bc3a8f8415a1507fe97b7f0ed13
Najat Ismail <46082009+ninaismail@users.noreply.github.com>
2024-09-17 13:16:04 +0300

    responsive fixes

--- src/components/Strategy.vue
+++ src/components/Strategy.vue
@@ -5,8 +5,8 @@
                 <h1 class="text-offwhite font-[600] 2xl:text-[68px] lg:text-7xl md:text-6xl sm:text-5xl text-4xl tracking-wide">Strategic Power in Action</h1>
                 <p class="text-offwhite font-[500] 2xl:text-[22px] lg:text-xl md:text-lg sm:text-md ">IBI Holding invests in tech companies and adds value to industries by nurturing new businesses and funding innovation initiatives that need the support to drive change.</p>
            </div>
-            <div class="flex flex-wrap items-center justify-between gap-y-10">
-                <div v-for="(item, key) in data" :key="key" class="relative flex flex-col justify-end md:px-6 2xl:w-[48%] lg:w-[49%] w-full h-full aspect-1.32/1">
+            <div class="flex flex-wrap items-center justify-center gap-6 gap-y-10">
+                <div v-for="(item, key) in data" :key="key" class="relative flex flex-col justify-end md:px-6 lg:w-[48%] w-full h-full aspect-1.32/1 last-of-type:place-items-center">
                     <img
                     :src="item.image"
                     alt="IBI Holding"
@@ -17,9 +17,8 @@
                     responsive
                     center cover
                     />
-                    <div class="relative bottom-0 p-6 space-y-6 cursor-pointer md:bottom-10 bg-bgcolor1/70" 
-                    @click="isOpen = key"
-                    >
+                    <div class="relative bottom-0 w-full p-6 space-y-6 cursor-pointer md:bottom-10 bg-bgcolor1/70" 
+                    @click="isOpen = key">
                         <hr class="absolute left-0 -top-1 bg-primary h-[10px] border-0 transition-all duration-1000"
                         :class=" isOpen === key ? 'w-2/3' : 'w-1/4'"/>
                         <div class="flex items-center justify-between gap-6 2xl:gap-10">
--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -39,7 +39,7 @@ onUnmounted(() => {
  :class="changeColor || isOpen === true ? 'bg-bgcolor shadow-lg' : 'bg-transparent'">
   <div class="relative items-center justify-center mx-auto 2xl:w-8/12 sm:w-10/12 lg:flex">
     <RouterLink id="Home" aria-label="go to homepage" to="/">
-      <img src="/logo.svg" alt="IBI Holding" width="241" height="76" center cover responsive loading="eager" class="aspect-3.17/1 max-lg:ps-[8.333333%] max-sm:ps-6"/>
+      <img src="/logo.svg" alt="IBI Holding" width="241" height="76" center cover responsive loading="eager" class="aspect-3.17/1 max-md:ps-[8.333333%] max-sm:ps-6"/>
     </RouterLink>
     <!-- Desktop -->
     <nav class="items-center justify-center hidden gap-3 ps-20 xl:gap-10 lg:w-10/12 lg:flex">
@@ -54,7 +54,7 @@ onUnmounted(() => {
     </nav> 
     <a prefetch="false" aria-label="contact us" href="tel:0097122222130" class="text-nowrap lg:block hidden font-[800] 2xl:text-[22px] lg:text-xl md:text-lg sm:text-md rounded-full top-4 w-fit cursor-pointer py-2 px-5 transition-all duration-400 text-center text-offwhite bg-primary hover:brightness-75">Contact Us</a>
     <!-- Mobile -->
-    <button aria-label="open menu" class="absolute flex items-center justify-center w-8 h-8 right-[8.333333%] lg:hidden top-6" @click="toggleOpen">
+    <button aria-label="open menu" class="absolute flex items-center justify-end w-8 h-8 md:right-0 right-[8.333333%] lg:hidden top-6" @click="toggleOpen">
       <svg v-if="!isOpen" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100%" height="100%" fill="white" viewBox="0 0 50 50">
       <path d="M 0 7.5 L 0 12.5 L 50 12.5 L 50 7.5 L 0 7.5 z M 0 22.5 L 0 27.5 L 50 27.5 L 50 22.5 L 0 22.5 z M 0 37.5 L 0 42.5 L 50 42.5 L 50 37.5 L 0 37.5 z"></path>
       </svg>   
@@ -63,7 +63,7 @@ onUnmounted(() => {
       </Transition> 
     </button>
     <Transition>
-    <nav v-show="isOpen" class="w-full py-10 max-lg:ps-[8.333333%] space-y-6 lg:hidden bg-bgcolor">
+    <nav v-show="isOpen" class="w-full py-10 max-md:ps-[8.333333%] space-y-6 lg:hidden bg-bgcolor">
       <div v-for="(item, key) in navlinks" :key="key">
         <RouterLink :id="item.name" :aria-label="'go to' + item.name" :to="item.to" 
         class="w-6/12 font-[500] 2xl:text-[22px] lg:text-xl md:text-lg sm:text-md cursor-pointer text-offwhite drop-shadow-md group">
